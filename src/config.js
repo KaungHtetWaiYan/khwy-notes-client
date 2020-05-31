ae30aa7eb1a425b6288362a6bee6b5157ba1f7dd
KaungHtetWaiYan <kaunghtet98wy@gmail.com>
2020-05-31 20:17:07 +0630

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "khwy-test-notes"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://v2gn7bin12.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_D3sE7GMbB",
+      APP_CLIENT_ID: "7rst899633dre7ke1s5t4lv7uj",
+      IDENTITY_POOL_ID: "us-west-2:c503aacf-0e8c-43f6-a440-4af44a549349"
+    }
+  };
